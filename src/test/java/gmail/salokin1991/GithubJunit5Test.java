e34736ec22393230bdb2032673f303eaa3575a7b
ShirleyFennette <152020270+ShirleyFennette@users.noreply.github.com>
2026-06-02 16:12:57 +0300

    Update GithubJunit5Test to extend BaseTest

--- src/test/java/gmail/salokin1991/GithubJunit5Test.java
+++ src/test/java/gmail/salokin1991/GithubJunit5Test.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 
-public class GithubJunit5Test {
+public class GithubJunit5Test extends BaseTest {
 
     @Test
     void firstTest(){
