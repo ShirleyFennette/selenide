d7ba225fabbb76458020b04b78693d12782e87e5
ShirleyFennette <152020270+ShirleyFennette@users.noreply.github.com>
2026-06-02 16:12:51 +0300

    Update DragAndDropTest to extend BaseTest

--- src/test/java/gmail/salokin1991/DragAndDropTest.java
+++ src/test/java/gmail/salokin1991/DragAndDropTest.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 
-public class DragAndDropTest {
+public class DragAndDropTest extends BaseTest {
 
     @Test
     void actionsTest(){
