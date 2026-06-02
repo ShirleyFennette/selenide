7c39974903bc584031c5c7bf34797c275487f05d
ShirleyFennette <152020270+ShirleyFennette@users.noreply.github.com>
2026-06-02 16:12:40 +0300

    Add BaseTest class with WebDriver initialization

---
+++ src/test/java/gmail/salokin1991/BaseTest.java
@@ -0,0 +1,20 @@
+package gmail.salokin1991;
+
+import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public class BaseTest {
+    @BeforeAll
+    static void setupWebDriver() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        Configuration.browser = "chrome";
+        Configuration.headless = true;
+        Configuration.timeout = 10000;
+    }
+}
